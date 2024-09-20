850c28ac91e2ec6e04306f0c7ce77f4be742d383
SeongJin Jeong <jsj0718tjdwl@naver.com>
2024-09-20 22:57:13 +0900

    lobby ui 배치.

---
+++ Assets/Scripts/Lobby/LobbyCanvas.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyCanvas : MonoBehaviour
+{
+    public List<Button> buttons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
