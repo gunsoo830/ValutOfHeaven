6c68f4573dcdb901cf3469deda8fd25d460eac23
SeongJin Jeong <jsj0718tjdwl@naver.com>
2024-09-25 17:21:07 +0900

    ExcelParser

--- Assets/Scripts/Lobby/LobbyManager.cs
+++ Assets/Scripts/Lobby/LobbyManager.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VOHUtil;
 
 public class LobbyManager : MonoBehaviour
 {
@@ -16,6 +18,16 @@ public class LobbyManager : MonoBehaviour
         {
             lobbyCanvasController = lobbyCanvas.GetComponent<LobbyCanvas>();
         }
+
+        List<List<string>> test = ExcelParser.getInstance().ParseExcel(
+            @"\Datasheet\CardData.xlsx",
+            0,
+            1,
+            5,
+            0,
+            7
+        );
+        Debug.Log(ExcelParser.getInstance().getDataWithType<string>(ref test, 0, 0));
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Util/ExcelParser.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Data;
+using ExcelDataReader;
+using System;
+
+namespace VOHUtil
+{
+    public class ExcelParser
+    {
+        private static ExcelParser instance = null;
+        public static ExcelParser getInstance()
+        {
+            if (instance != null)
+                return instance;
+
+            instance = new ExcelParser();
+            return instance;
+        }
+
+
+        ExcelParser()
+        {
+            RootPath = Directory.GetCurrentDirectory();
+        }
+
+        private string RootPath = "";
+
+        public List<List<string>> ParseExcel(string path, int sheetIndex, int startRow, int endRow, int startCol, int endCol)
+        {
+            return this._parseExcel(path, sheetIndex, startRow, endRow, startCol, endCol);
+        }
+        public List<List<string>> ParseExcel(string path, int sheetIndex, int row, int col)
+        {
+            return this._parseExcel(path, sheetIndex, row, row + 1, col, col+1);
+        }
+        private List<List<string>> _parseExcel(string path, int sheetIndex, int startRow, int endRow, int startCol, int endCol)
+        {
+            List<List<string>> retVal = new List<List<string>>();
+
+            string realPath = RootPath + path;
+
+            try
+            {
+                using (FileStream stream = File.Open(realPath, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    using (IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream))
+                    {
+                        DataSet result = reader.AsDataSet();
+                        DataTable Sheet = result.Tables[sheetIndex];
+
+                        for (int rowCount = startRow; rowCount < endRow; rowCount++)
+                        {
+                            List<string> rowData = new List<string>();
+                            for (int colCount = startCol; colCount < endCol; colCount++)
+                            {
+                                rowData.Add(Sheet.Rows[rowCount][colCount].ToString());
+                            }
+
+                            retVal.Add(rowData);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError(ex.ToString());
+            }
+
+            return retVal;
+        }
+
+        public T getDataWithType<T>(ref List<List<string>> excelData, int row, int col)
+        {
+            return (T) Convert.ChangeType(excelData[row][col], typeof(T));
+        }
+    }
+}
