b418924e411e7cf04ed8954e944771edffd0a316
SeongJin Jeong <jsj0718tjdwl@naver.com>
2024-10-14 23:47:03 +0900

    Battle Ing

--- Assets/Scripts/Battle/BattleManager.cs
+++ Assets/Scripts/Battle/BattleManager.cs
@@ -1,17 +1,38 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BattleManager : MonoBehaviour
 {
-    public delegate void TickEvent();
+    public enum TickEventType
+    {
+        Update = 0,
+        FireEvent,
+    }
+    public enum BattleShipType
+    {
+        Player = 0,
+        Enemy,
+        Count,
+        None
+    }
+    public enum BattleShipAttackType
+    {
+        Single = 0,
+        Multiple,
+        Random,
+    }
+
+
+    public delegate void TickEvent(float dt, TickEvent evtType);
 
     [Header("Battle Setting")]
-    public int tickTimeBySeconds = 60;
+    public float tickTimeBySeconds = 60;
     public bool isBattleStart = false;
     
     private float _elapsedTime = 0f;
-    private TickEvent _tickEventList;
+    private List<List<BattleShip>> _battleShipList = new List<List<BattleShip>>();
 
     // Start is called before the first frame update
     void Start()
@@ -19,10 +40,13 @@ public class BattleManager : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        for (int i = 0; i < (int)BattleShipType.Count; i++)
+        {
+            List<BattleShip> shipList = new List<BattleShip>();
+            this._battleShipList.Add(shipList);
+        }
     }
 
     public void PlayBattle()
@@ -30,26 +54,92 @@ public class BattleManager : MonoBehaviour
         this.isBattleStart = true;
     }
 
-    private void FixedUpdate()
+    public void StopBattle()
+    {
+        this.isBattleStart = false;
+    }
+
+    private void Update()
     {
         if (this.isBattleStart == false)
             return;
 
-        this._elapsedTime += Time.fixedDeltaTime;
-        if(this._elapsedTime > this.tickTimeBySeconds / 60)
+        float dt = Time.deltaTime;
+        this._elapsedTime += dt;
+        for(int i=0; i<(int)BattleShipType.Count; i++)
         {
-            this._elapsedTime = 0f;
-            this._fireTickEvent();
+            for(int shipindex=0; shipindex < this._battleShipList[i].Count; shipindex++)
+            {
+                this._battleShipList[i][shipindex].updateTick(dt / this.tickTimeBySeconds);
+            }
         }
     }
 
-    private void _fireTickEvent()
+    public void addBattleShip(BattleShip ship, BattleShipType shipType)
     {
-        this._tickEventList();
+        this._battleShipList[(int)shipType].Add(ship);
     }
 
-    public void addTickEvent(TickEvent evt)
+    // Attack
+    public void attackShip(float attackDmg, BattleShipType targetShipType, BattleShipAttackType attackType, int targetIndex = -1)
     {
-        this._tickEventList += evt;
+        switch(attackType)
+        {
+            case BattleShipAttackType.Single:
+                this._attackSingle(attackDmg, targetShipType, targetIndex);
+                break;
+            case BattleShipAttackType.Multiple:
+                //todo 나중에 배열로 받자....
+                break;
+            case BattleShipAttackType.Random:
+                this._attackRandom(attackDmg, targetShipType, 1);
+                break;
+        }
+        this._battleShipList[(int)BattleShipType.Enemy][0].getDamage(attackDmg);
+    }
+    private void _attackSingle(float dmg, BattleShipType targetType, int targetIndex)
+    {
+        this._battleShipList[(int)targetType][targetIndex].getDamage(dmg);
+    }
+    private void _attackRandom(float dmg, BattleShipType targetType, int attackCount = 1)
+    {
+        List<int> hitList = new List<int>();
+        int count = 0;
+        while(count < attackCount)
+        {
+            int targetIndex = Random.Range(0, this._battleShipList[(int)targetType].Count);
+            if (hitList.Find(x => x == targetIndex) != 0)
+                continue;
+            
+            this._attackSingle(dmg, targetType, targetIndex);
+            hitList.Add(targetIndex);
+            count++;
+        }
+    }
+
+    // Destroy
+    public void destroyShip(BattleShip ship, BattleShipType shipType)
+    {
+        int index = this._battleShipList[(int)shipType].FindIndex(ob => ob == ship);
+        if(index > -1)
+        {
+            this._battleShipList[(int)shipType].RemoveAt(index);
+            this._checkGameFinish();
+        }
+    }
+    private void _checkGameFinish()
+    {
+        BattleShipType loseType = BattleShipType.None;
+        for(int i=0; i<(int)BattleShipType.Count; i++)
+        {
+            if (this._battleShipList[i].Count <= 0)
+            {
+                loseType = (BattleShipType) i;
+                break;
+            }
+        }
+
+        if (loseType != BattleShipType.None)
+            this.StopBattle();
     }
 }
--- Assets/Scripts/Battle/BattleShip.cs
+++ Assets/Scripts/Battle/BattleShip.cs
@@ -1,6 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
 
 public class BattleShip : MonoBehaviour
@@ -11,13 +10,20 @@ public class BattleShip : MonoBehaviour
     [Space(10)]
 
     [Header("Ship Attributes")]
+    public BattleManager.BattleShipType shipType;
     public float turnSpeed = 1.0f;
     public float maxHealth = 1.0f;
+    public float attackDamage = 1.0f;
+    public float defenseVal = 1.0f;
 
+    private BattleManager _battleManager;
+
+    [SerializeField, ReadOnly]
     private float _currHealth = 0f;
-    private float _currTurnVal = 0f;
 
     private float _turnValPerTick = 0f;
+    private float _currTickVal = 0f;
+    private float _maxTick = 0f;
 
     private bool _isTurnGaugeFull = false;
 
@@ -25,8 +31,15 @@ public class BattleShip : MonoBehaviour
     void Start()
     {
         this._turnValPerTick = (float)Math.Round((this.turnSpeed * 0.07 / 100), 2);
+        this._maxTick = (float)Math.Round((1 / this._turnValPerTick), 2);
 
-        GameObject.FindAnyObjectByType<BattleManager>().addTickEvent(this.tickEvent);
+        this._currHealth = this.maxHealth;
+
+        this._battleManager = GameObject.FindAnyObjectByType<BattleManager>();
+        this._battleManager.addBattleShip(this, this.shipType);
+
+        this.setHealth();
+        this.setTurnMeter();
     }
 
     // Update is called once per frame
@@ -35,38 +48,83 @@ public class BattleShip : MonoBehaviour
         
     }
 
+    public void setHealth()
+    {
+        this.HealthGauge.SetPercent((this._currHealth / this.maxHealth) * 100);
+    }
     public void setHealth(float health)
     {
         this._currHealth = health;
-        this.HealthGauge.SetPercent(health/this.maxHealth);
+        this.HealthGauge.SetPercent((health/this.maxHealth) * 100);
     }
-    public void setTurnMeter(float meter)
+    public void setTurnMeter()
     {
-        this._currTurnVal = meter;
-        this.TurnMeterGauge.SetPercent(meter * 100);
+        this.TurnMeterGauge.SetPercent((this._currTickVal / this._maxTick) * 100);
+    }
+    public void setTurnMeter(float tickVal)
+    {
+        this.TurnMeterGauge.SetPercent((tickVal / this._maxTick) * 100);
     }
 
-    public void tickEvent()
+    // Tick Update
+    public void updateTick(float tick)
     {
+        if (this._isTurnGaugeFull == true)
+            return;
+
         Debug.Log("Tick!");
-        this._currTurnVal += this._turnValPerTick;
+        this._currTickVal += tick;
         this._updateTurnMeter();
     }
 
     private void _updateTurnMeter()
     {
-        if (this._isTurnGaugeFull == true)
-            return;
-
-        if (this._currTurnVal > 1)
+        if (this._currTickVal > this._maxTick)
             this._onTurnActivate();
         else
-            this.setTurnMeter(this._currTurnVal);
+            this.setTurnMeter();
     }
     private void _onTurnActivate()
     {
         this._isTurnGaugeFull = true;
-        this._currTurnVal = 0f;
-        this.setTurnMeter(1);
+        this.setTurnMeter(this._maxTick);
+        this._attackShip();
+    }
+
+    //Attack
+    private void _attackShip()
+    {
+        Debug.Log("Ship Attack!!");
+        //todo function ±¸Çö
+
+        BattleManager.BattleShipType targetType = BattleManager.BattleShipType.Enemy;
+        if (this.shipType == BattleManager.BattleShipType.Enemy)
+            targetType = BattleManager.BattleShipType.Player;
+
+        this._battleManager.attackShip(this.attackDamage, targetType, BattleManager.BattleShipAttackType.Random);
+
+        this._onAttackFinish();
+    }
+    private void _onAttackFinish()
+    {
+        this._isTurnGaugeFull = false;
+        this._currTickVal = 0f;
+    }
+
+    // Hit
+    public void getDamage(float damage)
+    {
+        this._currHealth -= damage;
+        this.setHealth();
+
+        if (this._currHealth < 0f)
+            this._onShipDestroy();
+    }
+
+    // Death
+    private void _onShipDestroy()
+    {
+        this._battleManager.destroyShip(this, this.shipType);
+        Destroy(this.gameObject);
     }
 }
